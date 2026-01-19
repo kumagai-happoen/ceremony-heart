1e4725759a64ce23e3922c92af9019f7b2f4ee7e
kumagai-happoen <kkumagai@happo-en.com>
2026-01-19 20:42:55 +0900

    Add files via upload

--- funeral-form/kintone-api.js
+++ funeral-form/kintone-api.js
@@ -1,6 +1,18 @@
-async function testWorkers() {
+async function loadRecord() {
+  const params = new URLSearchParams(location.search);
+  const conductId = params.get("id");
+
   const res = await fetch("https://ceremonyheart.kkumagai.workers.dev/");
-  const text = await res.text();
-  console.log("Workers response:", text);
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ conductId })
+  });
+
+  const kintoneData = await res.json();
+
+  // HTML に表示
+  document.querySelector("#deceased_name").textContent = kintoneData.record.故人名前.value;
+  document.querySelector("#deceased_furigana").textContent = kintoneData.record.故人ふりがな.value;
 }
-testWorkers();
+loadRecord();
+
